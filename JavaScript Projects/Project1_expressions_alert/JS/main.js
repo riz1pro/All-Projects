99ad34c77a59b984c3a3455b909b189aa46a8e2a
riz1pro <rizlapro1@live.co.uk>
2022-11-06 21:36:51 +0000

    java

---
+++ JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,29 @@
+
+window.alert("Hello World");
+
+var A = "this is a string";
+
+
+document.write(A);
+
+document.write("\"Be who you are and say what you feel,"
++"because those who mind don\`t matter and those who matter don\`t mind.\""
++"-Dr. Seuss");
+
+var family ="Bhatties", Dad="Najib", Mum="Rukhasana", Sister="Kani";
+
+document.write(Dad);
+
+var blues="I have the blues";
+var blues =blues.fontcolor("blue");
+document.write(blues);
+
+
+document.write(3+3);
+
+
+function My_First_Function () {   //defining a function and naming it
+    var str="This text is green!";
+    var result=str.fontcolor("green");
+    Document.getElementByld("Green_Text").innerHTML=result;
+}
