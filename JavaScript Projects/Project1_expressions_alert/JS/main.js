a38341ed82595d77a29e603f18d1770892dcba33
riz1pro <rizlapro1@live.co.uk>
2022-11-08 00:43:35 +0000

    SA 1

--- JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,25 +1,30 @@
 
 window.alert("Hello World");
 
-var A = "this is a string";
+var A = "this is a string"+"<br>";
 
 
 document.write(A);
 
-document.write("\"Be who you are and say what you feel,"
-+"because those who mind don\`t matter and those who matter don\`t mind.\""
-+"-Dr. Seuss");
+document.write("\"Be who you are and say what you feel," 
++ "because those who mind don\`t matter and those who matter don\`t mind.\""
++ "-Dr. Seuss"+"<br>");
 
-var family ="Bhatties", Dad="Najib", Mum="Rukhasana", Sister="Kani";
+var family ="Bhatties"+"<br>", Dad="Najib"+"<br>", Mum="Rukhasana"+"<br>", Sister="Kani"+"<br>";
 
-document.write(Dad);
+document.write(Mum);
 
-var blues="I have the blues";
+var blues="I have the blues"+"<br>";
 var blues =blues.fontcolor("blue");
 document.write(blues);
 
+var B = "concatenated"   +   "String"+"<br>";
+
+document.write(B)
+
 
 document.write(3+3);
+
 
 
 function My_First_Function () {   //defining a function and naming it
