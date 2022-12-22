d2711d816da9dba9900defa9dea7b9fd665de3f2
riz1pro <rizlapro1@live.co.uk>
2022-12-22 01:49:17 +0000

    re sub 9

--- JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -1,3 +1,27 @@
+function countdown() {
+  var seconds = document.getElementById("seconds").value;
+  
+  function tick() {
+    seconds = seconds -1;
+    timer.innerHTML = seconds;
+    setTimeout(tick, 1000);
+    if(seconds ===-1) {
+      alert("Times up!");
+    }
+       }
+  tick();
+}
+
+
+
+
+
+
+
+
+
+
+
 let slideIndex = 0;
 showSlides();
 
